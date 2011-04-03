f9d188f76684c03c70fca5327b51706fad40d65b
simoeln7@gmail.com <simoeln7@gmail.com@d57cbaf6-09dd-6b09-17d6-671ea8a26e02>
2011-04-03 10:52:26 +0000

    Offer creation

---
+++ trunk/shop/inc/page/offer/update.php
@@ -0,0 +1,32 @@
+<?php
+ include('../inc/global.config.php');
+
+$in = $_POST;
+
+$productId   = isset($in['productId'])   && filter_var($in['productId'], FILTER_VALIDATE_INT) && $in['productId'] > 0 ? $in['productId']   : null;
+$price        = isset($in["price"])        && is_numeric($in["price"])    && $in["price"]    > 0 ? stripslashes($in["price"])        : 0;
+$startTime = isset($in['startTime'])   && $in["startTime"]  != "" ? stripslashes($in["startTime"])  : "";
+$endTime = isset($in['endTime'])   && $in["endTime"]  != "" ? stripslashes($in["endTime"])  : "";
+//$displayOnlyImage = isset($in['displayOnlyImage'])   && $in['displayOnlyImage'] > 0 ? $in['displayOnlyImage']   : null;
+$displayOnlyImage;
+
+
+$result = 0;
+if ($productId != null && $price != null  && $startTime != null  && $endTime != null) { 
+  $productManager = new ProductManager();
+  $result  = $productManager->saveOffer(new Offer($productId, $price, $startTime, $endTime, $displayOnlyImage)); 
+ }
+ 
+ 
+
+echo <<< END
+<?xml version="1.0" encoding="utf-8"?>
+<r><result>$result</result></r>
+<r><productId>$productId</productId></r>
+<r><price>$price</price></r>
+<r><startTime>$startTime</startTime></r>
+<r><endTime>$endTime</endTime></r>
+
+END
+
+?>
