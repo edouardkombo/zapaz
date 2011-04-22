b43256f9d59d13aaa39420c989ffaa2706e3bd4f
fabienrenaud.fr <fabienrenaud.fr@d57cbaf6-09dd-6b09-17d6-671ea8a26e02>
2011-04-22 22:26:26 +0000

    getProducts with JSON now. Main KML created.

--- shop/www/getProducts.php
+++ shop/www/getProducts.php
@@ -1,6 +1,5 @@
 <?php
 
-header("Content-type:application/xml");
 include('../inc/global.config.php');
 
 $in = $_GET;
@@ -19,73 +18,22 @@ $root = $doc->appendChild($root);
 $shopManager = new ShopManager();
 $shop = $shopManager->getShopByPublicUid($publicUid);
 
+$products = array();
 if ($shop != null) {
   $shopId = $shop->getId();
 
   $productManager = new ProductManager();
-  $productResults = $productManager->getAllProducts($shopId);
-
-  foreach ($productResults as $prd) {
-    $product = $doc->createElement("product");
-    $product = $root->appendChild($product);
-
-    $id = $doc->createElement('id');
-    $id->appendChild($doc->createTextNode($prd->getId()));
-    $product->appendChild($id);
-
-    $name = $doc->createElement("name");
-    $name->appendChild(
-            $doc->createTextNode($prd->getName())
-    );
-    $product->appendChild($name);
-
-    $categoryId = $doc->createElement("categoryId");
-    $categoryId->appendChild(
-            $doc->createTextNode($prd->getCategoryId())
-    );
-    $product->appendChild($categoryId);
-
-    $typeId = $doc->createElement("typeId");
-    $typeId->appendChild(
-            $doc->createTextNode($prd->getTypeId())
-    );
-    $product->appendChild($typeId);
-
-    $shopId = $doc->createElement('shopId');
-    $shopId->appendChild($doc->createTextNode($prd->getShopId()));
-    $product->appendChild($shopId);
-
-    $manufactuer = $doc->createElement("manufacturer");
-    $manufactuer->appendChild(
-            $doc->createTextNode($prd->getManufacturer())
-    );
-    $product->appendChild($manufactuer);
-    
-    $price = $doc->createElement("price");
-    $price->appendChild(
-            $doc->createTextNode($prd->getPrice())
-    );
-    $product->appendChild($price);
-    
-    $description = $doc->createElement("description");
-    $description->appendChild(
-            $doc->createTextNode($prd->getDescription())
-    );
-    $product->appendChild($description);
-    
-    $picture = $doc->createElement("picture");
-    $picture->appendChild(
-            $doc->createTextNode($prd->getPicture())
-    );
-    $product->appendChild($picture);
-    
-  }
-
-// tell the browser what kind of file is come in
-} else {
-  //Send error message
+  $products = $productManager->getAllProducts($shopId);
 }
-header("Content-type: text/xml");
-echo $doc->saveXML();
+
+$json = "[";
+foreach ($products as $p) {
+  if ($json != "[")
+    $json .= ",";
+  $json .= $p->getJSON();
+}
+$json .= "]";
+echo $json;
+
 ?>
 
