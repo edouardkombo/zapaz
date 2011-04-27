b312d66ea12d7fdbe9a0b90ace1e27243655d1e4
fabienrenaud.fr <fabienrenaud.fr@d57cbaf6-09dd-6b09-17d6-671ea8a26e02>
2011-04-27 02:08:31 +0000

    Shop's pictures in shared database.

--- admin/inc/dao/ShopDao.php
+++ admin/inc/dao/ShopDao.php
@@ -87,10 +87,11 @@ class ShopDao {
     if ($shop == null) {
       return 0;
     }
-    $q = $this->db->prepare("INSERT INTO `".TABLE_SHOP."` (publicUid, name, currencyId, latitude, longitude, email, countOfProducts, creationTime, lastUpdate, webServiceUrl) VALUES (?,?,?,?,?,?,?,?,?,?)");
+    $q = $this->db->prepare("INSERT INTO `".TABLE_SHOP."` (publicUid, name, picture, currencyId, latitude, longitude, email, countOfProducts, creationTime, lastUpdate, webServiceUrl) VALUES (?,?,?,?,?,?,?,?,?,?,?)");
     $r = $q->execute(array(
       $shop->getPublicUid(),
       $shop->getName(),
+      $shop->getPicture(),
       $shop->getCurrencyId(),
       $shop->getLatitude(),
       $shop->getLongitude(),
@@ -114,10 +115,11 @@ class ShopDao {
     if ($shop == null) {
       return 0;
     }
-    $q = $this->db->prepare("UPDATE `".TABLE_SHOP."` SET publicUid = ?, name = ?, currencyId = ?, latitude = ?, longitude = ?, email = ?, countOfProducts = ?, creationTime = ?, lastUpdate = ?, webServiceUrl = ? WHERE id = ?");
+    $q = $this->db->prepare("UPDATE `".TABLE_SHOP."` SET publicUid = ?, name = ?, picture = ?, currencyId = ?, latitude = ?, longitude = ?, email = ?, countOfProducts = ?, creationTime = ?, lastUpdate = ?, webServiceUrl = ? WHERE id = ?");
     return $q->execute(array(
       $shop->getPublicUid(),
       $shop->getName(),
+      $shop->getPicture(),
       $shop->getCurrencyId(),
       $shop->getLatitude(),
       $shop->getLongitude(),
@@ -141,6 +143,7 @@ class ShopDao {
     $s = new Shop(
       $t["publicUid"],
       $t["name"],
+      $t["picture"],
       $t["currencyId"],
       $t["latitude"],
       $t["longitude"],
