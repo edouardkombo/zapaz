18e15a3eea9054c825ea26e055ba668de5404b09
simoeln7@gmail.com <simoeln7@gmail.com@d57cbaf6-09dd-6b09-17d6-671ea8a26e02>
2011-04-27 04:08:54 +0000

    Shop Info fixed

--- trunk/shop/www/getInfos.php
+++ trunk/shop/www/getInfos.php
@@ -6,10 +6,10 @@ include('../inc/global.config.php');
 $in = $_GET;
 
 //Add additional regex for expression for search terms
-$publicUid = isset($in['publicUid']) && $in['publicUid'] != '' ? $in['publicUid'] : 0;
+$publicUid = isset($in['publicUid']) && $in['publicUid'] != '' ? $in['publicUid'] : null;
 
 $result = 0;
-if ($publicUid > 0) {
+if ($publicUid != null) {
   $shopManager = new ShopManager();
   $shop = $shopManager->getShopByPublicUid($publicUid);
   
